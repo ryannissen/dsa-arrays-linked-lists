52b4061d4ee9ea0fab32bd7d693257f85ade8e28
Nate Snyder <n8.snyder@berkeley.edu>
2022-04-11 12:33:51 -0700

    Implemented insertAt, setAt, getAt

--- linked-list.js
+++ linked-list.js
@@ -72,25 +72,76 @@ class LinkedList {
   /** shift(): return & remove first item. */
 
   shift() {
+    let shifted = this.head;
+    this.length -= 1;
 
+    if (this.tail === shifted) this.tail = null;
+
+    this.head = shifted.next;
+
+    return shifted.val;
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-
+    let current = this.head;
+    let i = 0;
+    while (current !== null) {
+      if (i === idx) {
+        return current.val;
+      }
+      i += 1;
+      current = current.next;
+    }
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-
+    let current = this.head;
+    let i = 0;
+    while (current !== null) {
+      if (i === idx) {
+        current.val = val;
+      }
+      i += 1;
+      current = current.next;
+    }
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    const newNode = new Node(val);
+    
+    if (this.length === 0) {
+      this.tail = newNode;
+    }
 
+    if (idx === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+
+    // idx !== 0
+    let current = this.head;
+    let i = 0;
+    while (i < idx) {
+      if (i === idx - 1) {
+        let nextNode = current.next;
+        current.next = newNode;
+        newNode.next = nextNode;
+        if (this.tail === current) {
+          this.tail = newNode;
+        }
+      }
+      i += 1;
+      current = current.next;
+    }
+
+    this.length += 1;
   }
 
   /** removeAt(idx): return & remove item at idx, */
